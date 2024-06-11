28b62ece0cd29354077b58652136a558282a7f6d
Royce <roycelim4e@gmail.com>
2024-06-11 20:57:29 +0800

    added bootstrap and summary html

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "form", to: "form#index"
+  get "summary", to: "form#summary"
 
 
   # Defines the root path route ("/")

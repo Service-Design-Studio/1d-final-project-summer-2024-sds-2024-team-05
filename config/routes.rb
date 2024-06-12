570ed50fbdaf5658b22e57d8ffbf3bc1d2d75cf9
sneakiestofbeaks <hubird.hk@gmail.com>
2024-06-12 22:56:17 +0800

    Service Req + Med Cond html

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "form", to: "form#index"
+  get "form" => "form/index"
+  # root "form#index"
+  root "form#med_cond"
+  # root "form#serv_req"
   get "summary", to: "form#summary"
 
 

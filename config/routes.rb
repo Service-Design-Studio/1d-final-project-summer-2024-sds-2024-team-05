1bc159278302e984186303962632c81fb31d5684
Austin Isaac <vinuisaac@hotmail.com>
2024-06-13 23:58:04 +0800

    Update routes.rb (photos and videos uploading views)

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,20 @@ Rails.application.routes.draw do
   get "services/signup"=> "services#signup"
   get "services/envpics"=> "services#envpics"
   get "services/login"=> "services#login"
+  get 'photos/new'
+  get 'photos/create'
+  get 'videos/new'
+  get 'videos/create'
+  get 'photos/new', to: 'photos#new', as: 'new_photos'
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   # get "services" => ""
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :videos, only: [:new, :create]
+  resources :photos, only: [:new, :create]
+  root 'services#service'
+
 end

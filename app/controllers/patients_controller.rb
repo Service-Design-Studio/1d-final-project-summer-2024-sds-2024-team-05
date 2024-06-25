107a04ba7d00483fbe41e66f0d8998925a6088db
Royce <roycelim4e@gmail.com>
2024-06-25 16:25:57 +0800

    Able to navigate pages without saving

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,5 +1,5 @@
 class PatientsController < ApplicationController
-   before_action :set_form, only: [:show, :edit_1, :update_1, :edit_2, :update_2, :edit_3, :update_3]
+   before_action :set_form, only: [:show, :edit_1, :update_1, :edit_2, :update_2, :edit_3, :update_3, :edit_4, :update_4, :edit_5, :update_5]
 
   def index
     @forms = Form.select(:id, :first_name, :last_name, :start_date)
@@ -23,9 +23,13 @@ class PatientsController < ApplicationController
         redirect_to edit_1_form_path(@form), notice: 'Step 1 of form creation was successfully saved.'
       end
     when 'Next'
-      @form = Form.new(form_params_step1)
-      if @form.save
-        redirect_to edit_2_form_path(@form), notice: 'Step 1 of form creation was successfully saved.'
+      if params[:form].present? && params[:form].values.any?(&:present?)
+        @form = Form.new(form_params_step1)
+        if @form.save
+          redirect_to edit_2_form_path(@form), notice: 'Step 1 of form creation was successfully saved.'
+        end
+      else
+        redirect_to edit_2_forms_path, notice: 'Step 1 of form creation was successfully saved.'
       end
     else
       render :new
@@ -34,13 +38,11 @@ class PatientsController < ApplicationController
 
   # GET /forms/1/edit_1
   def edit_1
-    @form = Form.find(params[:id])
     @form_origin_text = determine_form_origin_text #Changes my header based on my origin new or edit
   end
 
   # PATCH /forms/1/update_1
   def update_1
-    @form = Form.find(params[:id])
     case params[:commit]
     when 'Save'
       if @form.update(form_params_step1)
@@ -54,16 +56,37 @@ class PatientsController < ApplicationController
       render :edit_1
     end
   end
+  
+  #collection update 1
+  def update_1_collection
+    case params[:commit]
+    when 'Save'
+      @form = Form.new(form_params_step1)
+      if @form.save
+        session[:form_origin] = 'new'
+        redirect_to edit_1_form_path(@form), notice: 'Step 1 of form creation was successfully saved.'
+      end
+    when 'Next'
+      if params[:form].present? && params[:form].values.any?(&:present?)
+        @form = Form.new(form_params_step1)
+        if @form.save
+          redirect_to edit_2_form_path(@form), notice: 'Step 1 of form creation was successfully saved.'
+        end
+      else
+        redirect_to edit_2_forms_path, notice: 'Step 1 of form creation was successfully saved.'
+      end
+    else
+      render :edit_1
+    end
+  end
 
   # GET /forms/1/edit_2
   def edit_2
-    @form = Form.find(params[:id])
     @form_origin_text = determine_form_origin_text #Changes my header based on my origin new or edit
   end
 
   # PATCH /forms/1/update_2
   def update_2
-    @form = Form.find(params[:id])
     case params[:commit]
     when 'Save'
       if @form.update(form_params_step2)
@@ -78,15 +101,36 @@ class PatientsController < ApplicationController
     end
   end
 
+    #collection update 2
+    def update_2_collection
+      case params[:commit]
+      when 'Save'
+        @form = Form.new(form_params_step2)
+        if @form.save
+          session[:form_origin] = 'new'
+          redirect_to edit_2_form_path(@form), notice: 'Step 2 of form creation was successfully saved.'
+        end
+      when 'Next'
+        if params[:form].present? && params[:form].values.any?(&:present?)
+          @form = Form.new(form_params_step2)
+          if @form.save
+            redirect_to edit_3_form_path(@form), notice: 'Step 2 of form creation was successfully saved.'
+          end
+        else
+          redirect_to edit_3_forms_path, notice: 'Step 2 of form creation was successfully saved.'
+        end
+      else
+        render :edit_2
+      end
+    end
+
   # GET /forms/1/edit_3
   def edit_3
-    @form = Form.find(params[:id])
     @form_origin_text = determine_form_origin_text #Changes my header based on my origin new or edit
   end
 
   # PATCH /forms/1/update_3
   def update_3
-    @form = Form.find(params[:id])
     case params[:commit]
     when 'Save'
       if @form.update(form_params_step3)
@@ -101,16 +145,37 @@ class PatientsController < ApplicationController
     end
   end
 
+  #collection update 1
+  def update_3_collection
+    case params[:commit]
+    when 'Save'
+      @form = Form.new(form_params_step3)
+      if @form.save
+        session[:form_origin] = 'new'
+        redirect_to edit_3_form_path(@form), notice: 'Step 2 of form creation was successfully saved.'
+      end
+    when 'Next'
+      if params[:form].present? && params[:form].values.any?(&:present?)
+        @form = Form.new(form_params_step3)
+        if @form.save
+          redirect_to edit_4_form_path(@form), notice: 'Step 2 of form creation was successfully saved.'
+        end
+      else
+        redirect_to edit_4_forms_path, notice: 'Step 2 of form creation was successfully saved.'
+      end
+    else
+      render :edit_3
+    end
+  end
+
   # GET /forms/1/edit_4
   def edit_4
-    @form = Form.find(params[:id])
     @form_origin_text = determine_form_origin_text #Changes my header based on my origin new or edit
   end
 
 # PATCH /forms/1/update_4
   def update_4
     puts 'test'
-    @form = Form.find(params[:id])
     Rails.logger.debug "params[:commit]: #{params[:commit]}"
     Rails.logger.debug "params[:form]: #{params[:form]}"
 
@@ -140,22 +205,61 @@ class PatientsController < ApplicationController
       end
       redirect_to edit_5_form_path(@form)
     else
-      puts 'hello'
       # Handle unexpected values for params[:commit]
       redirect_to edit_4_form_path(@form), alert: 'Invalid action.'
     end
   end
 
+  #collection update 1
+  def update_4_collection
+    case params[:commit]
+    when 'Upload Physical Video'
+      if params[:form].present? && params[:form][:physical_video].present?
+        @form = Form.new(form_params_step4)
+        @form.save
+        @form.physical_video.attach(params[:form][:physical_video])
+        # Redirect or render to update view to show the uploaded file
+        redirect_to edit_4_form_path(@form), notice: 'Physical video uploaded successfully.'
+      end
+    when 'Upload Mental Video'
+      if params[:form].present? && params[:form][:mental_video].present?
+        @form = Form.new(form_params_step4)
+        @form.save
+        @form.mental_video.attach(params[:form][:mental_video])
+        # Redirect or render to update view to show the uploaded file
+        redirect_to edit_4_form_path(@form), notice: 'Mental video uploaded successfully.'
+      end
+    when 'Save'
+      @form = Form.new()
+      @form.save
+      if params[:form].present?
+        @form.mental_video.attach(params[:form][:mental_video]) if params[:form][:mental_video].present?
+        @form.physical_video.attach(params[:form][:physical_video]) if params[:form][:physical_video].present?
+      end
+      redirect_to edit_4_form_path(@form)
+    when 'Next'
+      if params[:form].present?
+        @form = Form.new(form_params_step4)
+        @form.save
+        @form.mental_video.attach(params[:form][:mental_video]) if params[:form][:mental_video].present?
+        @form.physical_video.attach(params[:form][:physical_video]) if params[:form][:physical_video].present?
+        redirect_to edit_5_form_path(@form)
+      else
+        redirect_to edit_5_forms_path
+      end
+    else
+      # Handle unexpected values for params[:commit]
+      render :edit_4
+    end
+  end
 
   # GET /forms/1/edit_5
   def edit_5
-    @form = Form.find(params[:id])
     @form_origin_text = determine_form_origin_text # Changes my header based on my origin new or edit
   end
 
   # PATCH /forms/1/update_5
   def update_5
-    @form = Form.find(params[:id])
     Rails.logger.debug "params[:commit]: #{params[:commit]}"
     Rails.logger.debug "params[:form]: #{params[:form]}"
 
@@ -170,7 +274,7 @@ class PatientsController < ApplicationController
         @form.environment_video.attach(params[:form][:environment_video])
         redirect_to edit_5_form_path(@form), notice: 'Environment video uploaded successfully.'
       end
-    when 'Submit'
+    when 'Next'
       if params[:form].present?
         @form.environment_video.attach(params[:form][:environment_video]) if params[:form][:environment_video].present?
       end
@@ -180,6 +284,42 @@ class PatientsController < ApplicationController
       redirect_to edit_5_form_path(@form), alert: 'Invalid action.'
     end
   end
+
+  #collection update 1
+  def update_5_collection
+    case params[:commit]
+    when 'Upload Environment Video'
+      if params[:form].present? && params[:form][:environment_video].present?
+        @form.environment_video.attach(params[:form][:environment_video])
+        redirect_to edit_5_form_path(@form), notice: 'Environment video uploaded successfully.'
+      end
+    when 'Save'
+      @form = Form.new()
+      @form.save
+      if params[:form].present? && params[:form][:environment_video].present?
+        @form.environment_video.attach(params[:form][:environment_video])
+        redirect_to edit_5_form_path(@form), notice: 'Environment video uploaded successfully.'
+      end
+      redirect_to edit_4_form_path(@form)
+    when 'Next'
+      if params[:form].present?
+        @form = Form.new(form_params_step4)
+        @form.save
+        @form.environment_video.attach(params[:form][:environment_video]) if params[:form][:environment_video].present?
+        redirect_to @form
+      else
+        redirect_to show_error_forms_path
+      end
+    else
+      # Handle unexpected values for params[:commit]
+      render :edit_5
+    end
+  end
+
+  def show_error
+    render 'show_error'
+  end
+
 
 
   def determine_form_origin_text
@@ -201,7 +341,11 @@ class PatientsController < ApplicationController
   private
 
   def set_form
-    @form = Form.find(params[:id])
+    @form = if params[:id].present?
+      Form.find(params[:id])
+    else
+      Form.new
+    end
   end
 
   def form_params

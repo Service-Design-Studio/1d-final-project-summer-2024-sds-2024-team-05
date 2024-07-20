4c5a880affbe90a50fc03aedf9ab28cfe3a92a5f
Royce <roycelim4e@gmail.com>
2024-07-20 13:34:19 +0800

    Stopped tracking coverage

--- db/migrate/20240613030520_create_photos.rb
+++
@@ -1,9 +0,0 @@
-class CreatePhotos < ActiveRecord::Migration[7.1]
-  def change
-    create_table :photos do |t|
-      t.string :tag
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -80,12 +80,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_02_054328) do
     t.index ["user_id"], name: "index_forms_on_user_id"
   end
 
-  create_table "photos", force: :cascade do |t|
-    t.string "tag"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -101,11 +95,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_02_054328) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "videos", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"

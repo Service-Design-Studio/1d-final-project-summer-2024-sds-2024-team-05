7ea5b9b1d9a0507d6440f1d7eb00eab7cd3d4fb3
celine-goh <1007234@mymail.sutd.edu.sg>
2024-07-29 01:00:40 +0800

    changed home page, form pages, user nav bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,14 @@ class User < ApplicationRecord
   def admin?
     self.admin
   end
-  
+
+  def initials
+    if admin?
+      "NJ"
+    else
+      "#{user_first_name&.first || ''}#{user_last_name&.first || ''}".upcase
+    end
+  end
   validates :user_first_name, :user_last_name, :user_address, :user_postal, :user_contact_number, presence: true, unless: :admin?
   validates :email, presence: true, uniqueness: true
 end
